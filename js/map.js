c8036a550af0a25e96d5668c2817345de72aafdd
StrokovSt <30902163+StrokovSt@users.noreply.github.com>
2017-09-06 16:48:41 +0700

    Декоративные элементы v.0.7

---
+++ js/map.js
@@ -0,0 +1,11 @@
+  function initMap() {
+    var uluru = {lat: 34.8741737, lng: -111.7595701};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 9,
+      center: {lat: 34.6491367, lng: -111.7617045}
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
