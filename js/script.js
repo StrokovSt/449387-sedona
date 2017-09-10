58cb6bf1de46d2227437bed5bf7ec5b1695c2ad2
StrokovSt <30902163+StrokovSt@users.noreply.github.com>
2017-09-10 12:32:37 +0700

    Доработка

--- js/script.js
+++ js/script.js
@@ -33,13 +33,13 @@ window.addEventListener("keydown", function(evt) {
 });
 
 function initMap() {
-var uluru = {lat: 34.8741737, lng: -111.7595701};
-var map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 9,
-  center: {lat: 34.6491367, lng: -111.7617045}
-});
-var marker = new google.maps.Marker({
-  position: uluru,
-  map: map
-});
+  var uluru = {lat: 34.8741737, lng: -111.7595701};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 9,
+    center: {lat: 34.6491367, lng: -111.7617045}
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 };
